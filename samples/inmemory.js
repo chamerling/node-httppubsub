5d0c3e0aa708411c7869abf046d7fa258372b421
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-11-20 00:40:37 +0100

    Dummy version

---
+++ samples/inmemory.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * Copyright(c) 2012 Christophe Hamerling <chamerling@linagora.com>
+ * MIT Licensed
+ */
+
+var HTTPPubSub = require('../lib/httppubsub').HTTPPubSub;
+
+var pub = new HTTPPubSub();
+var resource = 'foo/bar';
+pub.subscribe(resource, 'http://localhost:3000/subscriber', function(err) {
+  if (err) {
+    console.log('Error while subscribing to resource ' + resource, err);
+    return -1;
+  }
+});
+
+pub.subscribe(resource, 'http://localhost:3000/subscriber2', function(err) {
+  if (err) {
+    console.log('Error while subscribing to resource ' + resource, err);
+    return -1;
+  }
+});
+
+// push to resource
+pub.publish(resource, {foo : 'bar', bar : 'foo'});
+
+
